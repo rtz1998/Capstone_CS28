1387a8ed9b9acd5f581361bc15f03c237a6fd9f7
rtz1998 <48934245+rtz1998@users.noreply.github.com>
2022-08-23 18:51:53 +1000

    Update DataLoad.py

--- DataLoad.py
+++ DataLoad.py
@@ -6,20 +6,21 @@ from transformers import BertTokenizer
 from PIL import Image
 from torchvision import transforms
 from torch.utils.data import DataLoader, TensorDataset
-import cv2 as cv
+
 
 class Dataloader(object):
-    def __init__(self, text_path='./all_train.tsv', image_path=''):
+    def __init__(self, text_path='', image_path=''):
         self.text_path = text_path
         self.image_path = image_path
 
     def read_text_image(self):
-        text = pd.read_csv(self.text_path, delimiter='\t')
-        col = ['clean_title', 'id', 'image_url', '2_way_label']
+        text = pd.read_csv(self.text_path)
+        col = ['clean_title', 'ids', 'image_url', '2_way_label']
         text = text[col]
         text = text.dropna(axis=0)
         txt = list(text['clean_title'])
-        id = np.array(text['id'])
+        id = np.array(text['ids'])
+        print(len(id))
         images = torch.zeros((len(txt), 3, 224, 224))
         for i in range(len(id)):
             image_path = os.path.join(self.image_path, str(id[i]) + '.jpeg')
@@ -39,8 +40,8 @@ class Dataloader(object):
         input_ids = list()
         token_type_ids = list()
         attention_mask = list()
-        for i in text:
-            tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        for n, i in enumerate(text):
             tokens = tokenizer.encode_plus(i, max_length=max_len, padding='max_length', truncation=True)
             input_ids.append(tokens['input_ids'])
             token_type_ids.append([tokens['token_type_ids']])
