ae73400938d0e91fd844b24396456a058c4c142b
rtz1998 <48934245+rtz1998@users.noreply.github.com>
2022-08-23 13:16:26 +1000

    Update DataLoad.py

--- DataLoad.py
+++ DataLoad.py
@@ -6,6 +6,7 @@ from transformers import BertTokenizer
 from PIL import Image
 from torchvision import transforms
 from torch.utils.data import DataLoader, TensorDataset
+import cv2 as cv
 
 class Dataloader(object):
     def __init__(self, text_path='./all_train.tsv', image_path=''):
@@ -13,25 +14,28 @@ class Dataloader(object):
         self.image_path = image_path
 
     def read_text_image(self):
-        text = pd.read_csv(self.text_path, delimiter='\t').head(10)
-        col = ['clean_title','id','image_url','2_way_label']
+        text = pd.read_csv(self.text_path, delimiter='\t').head()
+        col = ['clean_title', 'id', 'image_url', '2_way_label']
         text = text[col]
         text = text.dropna(axis=0)
         txt = list(text['clean_title'])
         id = np.array(text['id'])
-        images = list()
+        images = torch.zeros((len(txt), 3, 224, 224))
         for i in range(len(id)):
-            image_path = os.path.join(self.images_text, str(id[i]) + '.jpeg')
-            image = Image.open(image_path)
+            image_path = os.path.join(self.image_path, str(id[i]) + '.jpeg')
+            image = Image.open(image_path).convert('RGB')
             image = image.resize((224, 224))
-            images.append(image)
-        images = np.array(images)
+            transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                            transforms.RandomRotation((-45, 45)),
+                                            transforms.ToTensor(),
+                                            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.229, 0.224, 0.225))
+                                            ])
+            images[i,:,:,:] = transform(image)
         label = np.array(text['2_way_label'])
         return txt, images, label
-    
-    
+
     @staticmethod
-    def txt2tensor(self, text, max_len=512):
+    def txt2tensor(text, max_len=512):
         input_ids = list()
         token_type_ids = list()
         attention_mask = list()
@@ -45,31 +49,17 @@ class Dataloader(object):
         token_type_ids = torch.from_numpy(np.array(token_type_ids))
         attention_mask = torch.from_numpy(np.array(attention_mask))
         return input_ids, token_type_ids, attention_mask
-    
+
+
     @staticmethod
-    def img2tensor(self, images):
-        transform = transforms.Compose([transforms.RandomCrop(32,padding = 4),
-                                        transforms.RandomHorizontalFlip(),
-                                        transforms.RandomRotation((-45, 45)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.229, 0.224, 0.225))
-                                        ])
-        images = transform(images)
-        return images
-    
-    @staticmethod
-    def label2tensor(self, label):
+    def label2tensor(label):
         return torch.from_numpy(np.array(label))
 
     def loader(self):
         txt, images, labels = self.read_text_image()
         input_ids, token_type_ids, attention_mask = self.txt2tensor(txt)
-        images = self.img2tensor(images)
-        labels = self.label2tensor()
+        labels = self.label2tensor(labels)
         dataset = TensorDataset(input_ids, token_type_ids, attention_mask, images, labels)
-        loader = DataLoader(dataset=dataset, batch_size=64, shuffle=True, num_workers=0, drop_last=True)
+        loader = DataLoader(dataset=dataset, batch_size=2, shuffle=True, num_workers=0, drop_last=True)
 
         return loader
-
-
-
